723bf4c37f0de5b8bc161530d2ac09fdfcda1c93
David Jetelina <david@djetelina.cz>
2018-07-02 18:04:38 +0200

    Update script

---
+++ update.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Until I set up a proper CD, this is what I use to relaunch the bot
+git pull
+kill $(ps aux | grep '[b]in/python src/main.py' | awk '{print $2}')
+pipenv run python src/main.py &
+disown
