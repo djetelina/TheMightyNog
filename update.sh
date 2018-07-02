0481588ddb745004711e2c3649be5a50ec2f45b8
David Jetelina <david@djetelina.cz>
2018-07-02 18:11:02 +0200

    Add pipenv sync in case new dependencies appear

--- update.sh
+++ update.sh
@@ -2,5 +2,6 @@
 # Until I set up a proper CD, this is what I use to relaunch the bot
 git pull
 kill $(ps aux | grep '[b]in/python src/main.py' | awk '{print $2}')
+pipenv sync
 pipenv run python src/main.py &
 disown
